1341f7caea84715be8fb0a9f78f22d31a3b19081
lillie428 <33239852+lillie428@users.noreply.github.com>
2020-04-23 12:45:46 -0400

    Update util.c

--- project/util.c
+++ project/util.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "data_structs.h"
+#include "util.h"
 
 void visualize_graph(graph_t* graph){
     // Going through all the nodes 
@@ -38,5 +39,50 @@ void visualize_graph(graph_t* graph){
     }
     printf("\n\n\n");
     return;
-
 }
+
+graph_t* init_search_space(int width, int height, int *start, int *end){
+    printf("Start Init\n");
+    graph_t *graph = malloc(sizeof(graph_t));
+    graph->nodes = malloc(height * sizeof(node_t***));
+    for(int i =0; i<height; i++){
+        graph->nodes[i] = malloc(width * sizeof(node_t**));
+    }
+    // Going through all the nodes 
+    for(int i = 0; i<height; i++){
+        for(int j = 0; j<width; j++){
+            graph->nodes[i][j] = malloc(sizeof(node_t));
+            graph->nodes[i][j]->x = i;
+            graph->nodes[i][j]->y = j;
+            graph->nodes[i][j]->time = 0;
+            graph->nodes[i][j]->visited = 0;
+            if(i==start[0] && j==start[1]){
+                graph->nodes[i][j]->type = 'S';
+                graph->nodes[i][j]->f = 0;
+                graph->nodes[i][j]->g = 0;
+                graph->nodes[i][j]->h = 0;
+                graph->start = graph->nodes[i][j];
+
+            }
+            else if(i==end[0] && j==end[1]){
+                graph->nodes[i][j]->type = 'E';
+                graph->nodes[i][j]->f = 0;
+                graph->nodes[i][j]->g = 0;
+                graph->nodes[i][j]->h = 0;
+                graph->end = graph->nodes[i][j];
+            }
+            else{
+                graph->nodes[i][j]->type = 'O';
+            }
+        }
+    }
+    graph->width = width;
+    graph->height = height;
+    graph->start_node = start;
+    graph->end_node = end;
+  
+    printf("%d %d\n", graph->height, graph->width);
+    //visualize_graph(graph);
+    return graph; 
+}
+
