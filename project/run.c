b300818a8a950c305441a7ace9c04b1de16f78af
lillie428 <33239852+lillie428@users.noreply.github.com>
2020-04-23 12:37:13 -0400

    Update run.c

--- project/run.c
+++ project/run.c
@@ -4,7 +4,7 @@
 #include "run.h"
 #include "data_structs.h"
 #include "seq-search.h"
-
+#include "util.h"
 
 bool a_star(graph_t*);
 
@@ -14,46 +14,8 @@ void visualize_graph(graph_t* );
 
 double currentSeconds();
 
+void visualize_graph(graph_t* graph);
 
-
-void visualize_graph(graph_t* graph){
-    // Going through all the nodes 
-    printf("VISUALIZING\n");
-    printf("S is Start\n");
-    printf("E is End\n");
-    printf("V is Visited\n");
-    printf("0 is a regular node\n\n\n");
-    
-    printf("%d %d\n", graph->height, graph->width);
-    for(int i = 0; i<graph->height; i++){
-        for(int j = 0; j<graph->width; j++){
-            int t = graph->nodes[i][j]->time;
-            if(graph->nodes[i][j]==NULL)
-                printf("NULL\n");
-            if(j==graph->width-1){
-                if(t<10){
-                    printf("0%d\n", t);
-                }
-                else{
-                    printf("%d\n", t);
-                }
-                
-            }
-            else{
-                if(t<10){
-                    printf("0%d", t);
-                }
-                else{
-                    printf("%d", t);
-                }
-            }
-
-        }
-    }
-    printf("\n\n\n");
-    return;
-
-}
 bool parallel_a_star(graph_t* graph){
     double startTime = currentSeconds();
     node_t* start = graph->start; 
